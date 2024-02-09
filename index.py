042d7e9f9d860db9695fe3f8696aa204e78d1146
m-vp <mvenkatprashanth1@gmail.com>
2024-02-09 23:19:45 +0530

    FIRST COMMIT
    I have made a website that takes input and displays it. But, there is a problem here. for certain inputs some weird important data is displayed.

---
+++ index.py
@@ -0,0 +1,287 @@
+from flask import Flask, render_template, render_template_string, request
+import os
+app = Flask(__name__)
+
+app.config['MAX_COOKIE_SIZE'] = "flag{HaKuNa  (2nd part is in flag.txt file. it is in same directory as app.py)"
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    return render_template('error.html', message="Oops! The Payload U've Used is not correct"), 500
+
+
+
+@app.route('/')
+def index():
+    return render_template('indexx.html')
+
+
+@app.route('/check', methods=['POST'])
+def check():
+    
+    ip = request.form.get('ip', '')
+    
+    template = '''
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <style>
+            *, *:before, *:after {
+    box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    }
+
+    body {
+    background: linear-gradient(to right, #ea1d6f 0%, #eb466b 100%);
+    font-size: 12px;
+    }
+
+    body, button, input {
+    font-family: 'Montserrat', sans-serif;
+    font-weight: 700;
+    letter-spacing: 1.4px;
+    }
+
+    .background {
+    display: flex;
+    min-height: 100vh;
+    }
+
+    .container {
+    flex: 0 1 700px;
+    margin: auto;
+    padding: 10px;
+    }
+
+    .screen {
+    position: relative;
+    background: #3e3e3e;
+    border-radius: 15px;
+    }
+
+    .screen:after {
+    content: '';
+    display: block;
+    position: absolute;
+    top: 0;
+    left: 20px;
+    right: 20px;
+    bottom: 0;
+    border-radius: 15px;
+    box-shadow: 0 20px 40px rgba(0, 0, 0, .4);
+    z-index: -1;
+    }
+
+    .screen-header {
+    display: flex;
+    align-items: center;
+    padding: 10px 20px;
+    background: #4d4d4f;
+    border-top-left-radius: 15px;
+    border-top-right-radius: 15px;
+    }
+
+    .screen-header-left {
+    margin-right: auto;
+    }
+
+    .screen-header-button {
+    display: inline-block;
+    width: 8px;
+    height: 8px;
+    margin-right: 3px;
+    border-radius: 8px;
+    background: white;
+    }
+
+    .screen-header-button.close {
+    background: #ed1c6f;
+    }
+
+    .screen-header-button.maximize {
+    background: #e8e925;
+    }
+
+    .screen-header-button.minimize {
+    background: #74c54f;
+    }
+
+    .screen-header-right {
+    display: flex;
+    }
+
+    .screen-header-ellipsis {
+    width: 3px;
+    height: 3px;
+    margin-left: 2px;
+    border-radius: 8px;
+    background: #999;
+    }
+
+    .screen-body {
+    display: flex;
+    }
+
+    .screen-body-item {
+    flex: 1;
+    padding: 50px;
+    }
+
+    .screen-body-item.left {
+    display: flex;
+    flex-direction: column;
+    }
+
+    .app-title {
+    display: flex;
+    flex-direction: column;
+    position: relative;
+    color: #ea1d6f;
+    font-size: 15px;
+    }
+
+
+    .app-contact {
+    margin-top: auto;
+    font-size: 8px;
+    color: #888;
+    }
+
+    .app-form-group {
+    margin-bottom: 15px;
+    }
+
+    .app-form-group.message {
+    margin-top: 40px;
+    }
+
+    .app-form-group.buttons {
+    margin-bottom: 0;
+    text-align: right;
+    }
+
+    .app-form-control {
+    width: 100%;
+    padding: 10px 0;
+    background: none;
+    border: none;
+    border-bottom: 1px solid #666;
+    color: #ddd;
+    font-size: 14px;
+    text-transform: uppercase;
+    outline: none;
+    transition: border-color .2s;
+    }
+
+    .app-form-control::placeholder {
+    color: #666;
+    }
+
+    .app-form-control:focus {
+    border-bottom-color: #ddd;
+    }
+
+    .app-form-button {
+    background: none;
+    border: none;
+    color: #ea1d6f;
+    font-size: 14px;
+    cursor: pointer;
+    outline: none;
+    }
+
+    .app-form-button:hover {
+    color: #b9134f;
+    }
+
+
+
+    .credits-link {
+    display: flex;
+    align-items: center;
+    color: #fff;
+    font-weight: bold;
+    text-decoration: none;
+    }
+
+    .dribbble {
+    width: 20px;
+    height: 20px;
+    margin: 0 5px;
+    }
+
+    @media screen and (max-width: 520px) {
+    .screen-body {
+        flex-direction: column;
+    }
+
+    .screen-body-item.left {
+        margin-bottom: 30px;
+    }
+
+    .app-title {
+        flex-direction: row;
+    }
+
+    .app-title span {
+        margin-right: 12px;
+    }
+
+    .app-title:after {
+        display: none;
+    }
+    }
+
+    @media screen and (max-width: 600px) {
+    .screen-body {
+        padding: 40px;
+    }
+
+    .screen-body-item {
+        padding: 0;
+    }
+    }
+
+        </style>
+    </head>
+    <body>
+        
+        <form action="/check" method="post">
+            <div class="background">
+                <div class="container">
+                <div class="screen">
+                    <div class="screen-header">
+                    <div class="screen-header-left">
+                        <div class="screen-header-button close"></div>
+                        <div class="screen-header-button maximize"></div>
+                        <div class="screen-header-button minimize"></div>
+                    </div>
+                    <div class="screen-header-right">
+                        <div class="screen-header-ellipsis"></div>
+                        <div class="screen-header-ellipsis"></div>
+                        <div class="screen-header-ellipsis"></div>
+                    </div>
+                    </div>
+                    <div class="screen-body">
+                    <div class="screen-body-item center">
+                        <div class="app-title">
+                        <span>''' +ip+'''</span>
+                        </div>
+                    </div>
+                    
+                    </div>
+                    </div>
+                </div>
+                
+                </div>
+            </div>
+        </form>
+        
+    </body>
+    </html>'''
+    return render_template_string(template)
+if __name__ == '__main__':
+    app.run(debug=False, port=3000)
